11e91b80dbe65a2a24363389a000eae98cde3352
Prakhar Srivastav <prakhar1989@gmail.com>
2015-03-24 09:09:27 +0300

    added a css build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,26 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var reactify = require('reactify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var webserver = require('gulp-webserver');
+    browserify = require('browserify'),
+    reactify = require('reactify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    webserver = require('gulp-webserver'),
+    concat = require('gulp-concat'),
+    minifyCSS = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
+var fileOrder = [
+  'css/vendor/normalize.css',
+  'css/vendor/skeleton.css',
+  'css/main.css'
+];
+
+gulp.task('css', function() {
+    gulp.src(fileOrder)
+        .pipe(concat('styles.css'))
+        .pipe(minifyCSS())
+        .pipe(rename('styles.min.css'))
+        .pipe(gulp.dest('build'))
+});
 
 gulp.task('browserify', function() {
     return browserify('./js/app.js')
@@ -26,6 +43,6 @@ gulp.task('watch', function() {
   gulp.watch('./js/**/*.js', ['browserify']);
 });
 
-gulp.task('build', ['browserify']);
+gulp.task('build', ['browserify', 'css']);
 gulp.task('dev', ['watch', 'browserify']);
 gulp.task('default', ['webserver']);
