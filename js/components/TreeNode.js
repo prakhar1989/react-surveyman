556259d1c253e4119225ffce0618d526e2e296ea
Prakhar Srivastav <prakhar1989@gmail.com>
2015-06-12 15:04:57 +0530

    Restyling using itemtypes. #5

--- js/components/TreeNode.js
+++ js/components/TreeNode.js
@@ -1,10 +1,12 @@
 var React = require('react');
 var cx = require('classnames');
+var ItemTypes = require('./ItemTypes');
 
 var TreeNode = React.createClass({
     propTypes: {
         collapsed: React.PropTypes.bool,
         defaultCollapsed: React.PropTypes.bool,
+        type: React.PropTypes.oneOf([ItemTypes.BLOCK, ItemTypes.QUESTION]).isRequired,
         label: React.PropTypes.node.isRequired
     },
     getInitialState() {
@@ -26,13 +28,17 @@ var TreeNode = React.createClass({
             'ion-arrow-down-b': !collapsed,
             'ion-arrow-right-b': collapsed
         });
+        var containerClass = cx({
+            'tree-view_node-block': props.type === ItemTypes.BLOCK,
+            'tree-view_node-question': props.type === ItemTypes.QUESTION
+        });
 
         var arrow = (<div onClick={this.handleCollapse} className="tree-view_arrow">
                         <i className={arrowClass}></i>
                     </div>);
 
         return (
-           <div> {arrow}
+           <div className={containerClass}> {arrow}
                 <span onClick={this.handleClick}>{props.label}</span>
                 { collapsed ? null : <div className="tree-view_children">{this.props.children}</div> }
            </div>
--- js/components/TreeView.js
+++ js/components/TreeView.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var TreeNode = require('./TreeNode');
 var { List } = require('immutable');
+var ItemTypes = require('./ItemTypes');
 
 var TreeView = React.createClass({
     propTypes: {
@@ -20,15 +21,15 @@ var TreeView = React.createClass({
         // build the tree
         var tree = survey.map((block, i) => {
             var questions = block.get('questions');
-            var blockLabel = <span className="tree-view_node-block">{"Block #" +  block.get('id')}</span>;
+            var blockLabel = <span className="tree-view_block-title">{"Block #" +  block.get('id')}</span>;
             return (
-                <TreeNode key={i} label={blockLabel}>
+                <TreeNode key={i} label={blockLabel} type={ItemTypes.BLOCK}>
                 {questions.map((ques, j) => {
-                    var quesLabel = <span className="tree-view_node-question">{self.ellipsize(ques.get('qtext'))}</span>;
+                    var quesLabel = <span className="tree-view_question-title">{self.ellipsize(ques.get('qtext'))}</span>;
                     return (
-                        <TreeNode key={j} label={quesLabel} defaultCollapsed={false}>
+                        <TreeNode key={j} label={quesLabel} type={ItemTypes.QUESTION}>
                             <div className="tree-view_node">{"Options: " + ques.get('options').count()}</div>
-                            <div className="tree-view_node">{self.renderProp('randomizable', ques.get('randomizable'))}</div>
+                            <div className="tree-view_node">{self.renderProp('ordering', ques.get('ordering'))}</div>
                             <div className="tree-view_node">{self.renderProp('exclusive', ques.get('exclusive'))}</div>
                             <div className="tree-view_node">{self.renderProp('freetext', ques.get('freetext'))}</div>
                         </TreeNode>
