3300ba47e1f695eba1d0a78189b8ec1e06370e65
Prakhar Srivastav <prakhar1989@gmail.com>
2015-06-12 14:22:20 +0530

    moved code into functions. #5

--- js/components/TreeView.js
+++ js/components/TreeView.js
@@ -7,22 +7,26 @@ var TreeView = React.createClass({
         survey: React.PropTypes.instanceOf(List)
     },
     renderProp(label, prop) {
-        return prop ? <span>{label + ": True"}</span> : 
-                      <span>{label + ": False"}</span>
+        return prop ? <span><i className="ion-checkmark"></i> {label}</span> : 
+                      <span><i className="ion-close"></i>  {label}</span>
+    },
+    ellipsize(text) {
+        return text.substr(0, 20) + (text.length > 20 ? "..." : "");
     },
     render() {
         var { survey } = this.props;
         var self = this;
 
         // build the tree
-        var tree = survey.map(function(block, i) {
+        var tree = survey.map((block, i) => {
             var questions = block.get('questions');
+            var blockLabel = <span className="tree-view_node-block">{"Block #" +  block.get('id')}</span>;
             return (
-                <TreeNode key={i} label={"Block #" + block.get('id')}>
-                {questions.map(function(ques, j) {
-                    var label = <span className="node">{ques.get('qtext').substr(0, 20)}</span>;
+                <TreeNode key={i} label={blockLabel}>
+                {questions.map((ques, j) => {
+                    var quesLabel = <span className="tree-view_node-question">{self.ellipsize(ques.get('qtext'))}</span>;
                     return (
-                        <TreeNode key={j} label={label} defaultCollapsed={false}>
+                        <TreeNode key={j} label={quesLabel} defaultCollapsed={false}>
                             <div className="tree-view_node">{"Options: " + ques.get('options').count()}</div>
                             <div className="tree-view_node">{self.renderProp('randomizable', ques.get('randomizable'))}</div>
                             <div className="tree-view_node">{self.renderProp('exclusive', ques.get('exclusive'))}</div>
