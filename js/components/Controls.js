b842fad34d0e8b077d2b6d4a2717e4f34f364590
Prakhar Srivastav <prakhar1989@gmail.com>
2015-07-19 14:37:45 +0300

    Added button groups for save and load. #23

--- js/components/Controls.js
+++ js/components/Controls.js
@@ -8,15 +8,23 @@ var Controls = React.createClass({
     render() {
         return (
             <div className="row controls">
-                <div className="col-xs-9">
+                <div className="col-xs-8">
+                  <span className="help-text">Survey Builder Area</span>
                   <h3>Pallet</h3>
                 </div>
-                <div className="col-xs-3">
-                  <button
-                        className="btn btn-success btn-sm"
-                        onClick={this.handleDownload}>
-                      <span className="ion-arrow-down-a"></span> Download
-                  </button>
+                <div className="col-xs-4">
+                  <span className="help-text">Survey Actions</span>
+                  <div className="btn-group btn-group-sm" role="group">
+                      <button type="button" className="btn btn-default">
+                          <span className="ion-android-cloud-done"></span> Save
+                      </button>
+                      <button type="button" className="btn btn-default">
+                          <span className="ion-android-upload"></span> Load
+                      </button>
+                      <button type="button" className="btn btn-default" onClick={this.handleDownload}>
+                          <span className="ion-archive"></span> Download
+                      </button>
+                  </div>
                 </div>
             </div>
         );
