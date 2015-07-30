993423d72abc78500310661e872c116db2cc856b
Prakhar Srivastav <prakhar1989@gmail.com>
2015-07-31 02:18:47 +0530

    rebuilding app Fri Jul 31 02:18:47 IST 2015

--- release.sh
+++ release.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 npm run build
-git add -A
 msg="rebuilding app `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
+git commit -am "$msg"
 git push origin gh-pages
